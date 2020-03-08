30104a086168a429ad1236d21f910b55a16d50ed
pramodchoudhari <pramodchoudhari@live.com>
2020-03-08 10:19:37 +0530

    3 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { AdvanceSearchComponent} from './components/advance-search.component';
+import { ListingComponent} from './components/listing.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,ListingComponent,AdvanceSearchComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/components/listing.component.ts
+++ src/app/components/listing.component.ts
@@ -2,31 +2,30 @@ import { Component, OnInit, Injector, Inject } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ListingService } from './listing-service';
 
+
 @Component({
-    selector: 'advance-search',
+    selector: 'listing',
     template: `
-        <h1>Adavance search component</h1>
+        <h1>Component One</h1>
         <p>{{message}}</p>
         <button (click)="otherMessage()">New Message</button>
 
     `,
 })
-export class AdvanceSearchComponent {
+export class ListingComponent {
   message: string;
+  constructor(@Inject(ListingService) private listingService){}
 
-  constructor(@Inject(ListingService) private listingService){
-  }
-
-  ngOnInit() {
+ ngOnInit() {
   //subscribe in the component to the current message
    this.listingService.currentMessage.subscribe( message => {
    this.message = message;
    })
-  }
-  
-  otherMessage(){
-    this.listingService.updateMessage("Hello from advance search");
-  }
+ }
 
+ otherMessage(){
+   this.listingService.updateMessage("Hello from component listing")
+
+  }
 
 }
